5b7ac25386a099ae2cd78b03dca7cdb3c37e8f84
Janmm14 <computerjanimaus@yahoo.de>
2016-04-05 18:33:09 +0200

    Combine CobWebModule to one class as its not as much that a split is needeed

--- src/main/java/de/janmm14/epicpvp/warz/cobwebs/CobWebModule.java
+++ src/main/java/de/janmm14/epicpvp/warz/cobwebs/CobWebModule.java
@@ -1,15 +1,48 @@
 package de.janmm14.epicpvp.warz.cobwebs;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
 import de.janmm14.epicpvp.warz.Module;
 import de.janmm14.epicpvp.warz.WarZ;
 
-public class CobWebModule extends Module<CobWebModule> {
+public class CobWebModule extends Module<CobWebModule> implements Listener {
 
 	public CobWebModule(WarZ plugin) {
-		super( plugin, CobWebDigListener::new, CobWebShearsListener::new );
+		super( plugin, (module) -> module );
 	}
 
 	@Override
 	public void reloadConfig() {
+	}
+
+	@EventHandler
+	public void onInteract(PlayerInteractEvent event) {
+		if ( event.hasBlock() && event.hasItem() ) {
+			ItemStack item = event.getItem();
+			Block clickedBlock = event.getClickedBlock();
+			if ( item.getType() == Material.SHEARS && clickedBlock.getType() == Material.WEB ) {
+				clickedBlock.setType( Material.AIR, false );
+				if ( item.getDurability() > 1 ) {
+					item.setDurability( ( short ) ( item.getDurability() - 1 ) );
+					event.getPlayer().setItemInHand( item );
+				} else {
+					event.getPlayer().setItemInHand( null );
+				}
+				event.getPlayer().updateInventory();
+			}
+		}
+	}
+
+	@EventHandler(ignoreCancelled = true)
+	public void onBlockBreak(BlockBreakEvent event) {
+		if ( event.getBlock().getType() == Material.WEB ) {
+			event.setCancelled( true );
+		}
 	}
 }
