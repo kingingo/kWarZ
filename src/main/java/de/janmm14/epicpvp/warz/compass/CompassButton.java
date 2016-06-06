640439c30eebebf0d7c84cbd997ff3e544e1591c
kingingo <kingingohd@googlemail.com>
2016-06-06 23:06:45 +0200

    Compass Selection Inv

---
+++ src/main/java/de/janmm14/epicpvp/warz/compass/CompassButton.java
@@ -0,0 +1,43 @@
+package de.janmm14.epicpvp.warz.compass;
+
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import eu.epicpvp.kcore.Inventory.InventoryPageBase;
+import eu.epicpvp.kcore.Inventory.Inventory.InventoryCopy;
+import eu.epicpvp.kcore.Inventory.Item.Click;
+import eu.epicpvp.kcore.Inventory.Item.Buttons.ButtonCopy;
+import eu.epicpvp.kcore.Util.UtilEvent.ActionType;
+import eu.epicpvp.kcore.Util.UtilInv;
+import eu.epicpvp.kcore.Util.UtilItem;
+
+public class CompassButton extends ButtonCopy{
+
+	public CompassButton(CompassTargetModule module,CompassTarget target,int slot, ItemStack item,InventoryCopy inv) {
+		super(new Click(){
+
+			@Override
+			public void onClick(Player player, ActionType type, Object object) {
+				CompassTarget ptarget = module.getCompassTarget( player );
+				
+				if(ptarget==target){
+					((InventoryPageBase)object).setItem(slot, UtilItem.addEnchantmentGlow(item.clone()));
+				}
+			}
+			
+		},new Click(){
+
+			@Override
+			public void onClick(Player player, ActionType type, Object object) {
+				CompassTarget ptarget = module.getCompassTarget( player );
+				
+				if(ptarget!=target){
+					module.setCompassTarget(player, target);
+					inv.open(player, UtilInv.getBase());
+				}
+			}
+			
+		}, item);
+	}
+
+}
--- src/main/java/de/janmm14/epicpvp/warz/compass/CompassTargetSwitchListener.java
+++ src/main/java/de/janmm14/epicpvp/warz/compass/CompassTargetSwitchListener.java
@@ -1,17 +1,35 @@
 package de.janmm14.epicpvp.warz.compass;
 
 import org.bukkit.Material;
+import org.bukkit.SkullType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
+import eu.epicpvp.kcore.Inventory.Inventory.InventoryCopy;
+import eu.epicpvp.kcore.Util.InventorySize;
+import eu.epicpvp.kcore.Util.UtilInv;
+import eu.epicpvp.kcore.Util.UtilItem;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 public class CompassTargetSwitchListener implements Listener {
 
 	private final CompassTargetModule module;
+	private InventoryCopy selectionInventory;
+	
+	public CompassTargetSwitchListener(CompassTargetModule module){
+		this.module=module;
+		
+		this.selectionInventory=new InventoryCopy(InventorySize._9, "Set your Compass target");
+		this.selectionInventory.addButton(1, new CompassButton(module, CompassTarget.ENEMY, 1, UtilItem.Item(new ItemStack(Material.SKULL_ITEM,1,(byte)SkullType.ZOMBIE.ordinal()), new String[]{}, "§cEnemies"), this.selectionInventory));
+		this.selectionInventory.addButton(4, new CompassButton(module, CompassTarget.ENEMY, 4, UtilItem.Item(new ItemStack(Material.SKULL_ITEM,1,(byte)SkullType.PLAYER.ordinal()), new String[]{}, "§aFriends"), this.selectionInventory));
+		this.selectionInventory.addButton(7, new CompassButton(module, CompassTarget.ENEMY, 7, UtilItem.Item(new ItemStack(Material.FENCE), new String[]{}, "§6Zones"), this.selectionInventory));
+		this.selectionInventory.fill(Material.STAINED_GLASS_PANE, 7);
+		this.selectionInventory.setCreate_new_inv(true);
+		UtilInv.getBase().addPage(this.selectionInventory);
+	}
 
 	@EventHandler
 	public void onInteract(PlayerInteractEvent event) {
@@ -21,7 +39,6 @@ public class CompassTargetSwitchListener implements Listener {
 	}
 
 	private void openSelectionInventory(Player plr) {
-		CompassTarget target = module.getCompassTarget( plr );
-		//TODO implement
+		this.selectionInventory.open(plr, UtilInv.getBase());
 	}
 }
