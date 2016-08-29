f82cfd937c72cc6ac8fd30103e74577d7f65db3f
Janmm14 <computerjanimaus@yahoo.de>
2016-08-29 15:05:59 +0200

    Implement LanguageConverter

--- src/main/java/de/janmm14/epicpvp/warz/hooks/LanguageConverter.java
+++ src/main/java/de/janmm14/epicpvp/warz/hooks/LanguageConverter.java
@@ -1,12 +1,12 @@
 package de.janmm14.epicpvp.warz.hooks;
 
-import java.util.Arrays;
-
 import org.bukkit.entity.Player;
+
+import eu.epicpvp.kcore.Translation.TranslationHandler;
 
 public class LanguageConverter {
 
 	public String getLocalized(Player target, String identifier, Object... arguments) {
-		return "<missing translation '" + identifier + "' " + Arrays.toString( arguments ) + ">";
+		return TranslationHandler.getText( target, identifier, arguments );
 	}
 }
