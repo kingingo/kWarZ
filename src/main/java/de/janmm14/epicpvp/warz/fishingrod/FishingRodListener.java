450bac01b97de792840a4a2613f0cd39f9986ee9
kingingo <kingingohd@googlemail.com>
2016-09-10 11:02:00 +0200

    disable at the Spawn -> FishingRod

--- src/main/java/de/janmm14/epicpvp/warz/fishingrod/FishingRodListener.java
+++ src/main/java/de/janmm14/epicpvp/warz/fishingrod/FishingRodListener.java
@@ -16,7 +16,10 @@ import org.bukkit.util.Vector;
 
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
+import com.sk89q.worldguard.protection.flags.DefaultFlag;
+import com.sk89q.worldguard.protection.flags.StateFlag;
 
+import eu.epicpvp.kcore.Util.UtilWorldGuard;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -32,7 +35,8 @@ public class FishingRodListener implements Listener {
 
 	@EventHandler
 	public void onFishingRod(PlayerFishEvent event) {
-		if ( event.getState() == PlayerFishEvent.State.IN_GROUND || event.getState() == PlayerFishEvent.State.FAILED_ATTEMPT ) {
+		if ( (event.getState() == PlayerFishEvent.State.IN_GROUND || event.getState() == PlayerFishEvent.State.FAILED_ATTEMPT) 
+				&& !UtilWorldGuard.RegionFlag(event.getPlayer(), DefaultFlag.PVP)) {
 			Location location = event.getHook().getLocation();
 			boolean cancel = true;
 			for ( BlockFace face : BlockFace.values() ) {
